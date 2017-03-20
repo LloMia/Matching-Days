61ca287c214ffcb2c3aa2d23ae47f227291c01f7
Loyiso <Loyiso@projectcodex.co>
2017-03-20 12:17:57 +0200

    first commit

---
+++ matching.js
@@ -0,0 +1,11 @@
+var input1= document.querySelector("#input1");
+var input2=document.querySelector("#input2");
+
+
+function matchDate(){
+var inputDate = new Date(input1.value)
+var day = inputDate.getDay()
+var weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+}
